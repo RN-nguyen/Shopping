0a54560d9f6403199201044abf1ea482711f2b74
Rebecca Nguyen <onguyen89@gmail.com>
2014-04-01 22:33:06 -0600

    Initial scaffold for Product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopping::Application.config.secret_key_base = 'ec3cd2f15656ff21994c2c29c05edcd22a3813179959b301e5b3bfec5886c9395d4481d5d9aa8fc04e5d34c8e022cc9930e5b5ca6db0fcddcb9702314ef8107d'
